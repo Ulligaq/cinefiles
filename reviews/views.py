2d01b891692875eed807b7e0b77f15cab1981e03
Jake-M-Short <jakemshort@yahoo.com>
2023-11-12 07:24:57 -0700

    Adding Create View as well as sign out and create profile
    Adding sign up and sign out for accounts

--- reviews/views.py
+++ reviews/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.views.generic import ListView, DetailView
+from django.views.generic.edit import CreateView
 from .models import Review
 
 # Create your views here.
@@ -16,3 +17,9 @@ class ReviewDetailView(DetailView):
         context = super(ReviewDetailView, self).get_context_data(*args, **kwargs)
         context['review_list'] = Review.objects.all()
         return context
+    
+
+class ReviewCreateView(CreateView):
+    model = Review
+    template_name = "reviewCreate.html"
+    fields = ["movie", "thumbnail", "title", "content", "date", "author"]
